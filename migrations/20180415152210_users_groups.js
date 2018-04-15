0ea324f06edf8923636fb1c69422ecf0c4ddbf41
Mitch Lembke <mitchell.lembke@gmail.com>
2018-04-15 15:53:27 -0600

    Added groups and users_groups tables.

--- migrations/20180415152210_users_groups.js
+++ migrations/20180415152210_users_groups.js
@@ -1,12 +1,12 @@
 exports.up = (knex, Promise) => {
-  return knex.schema.createTable('users_timelines', (table) => {
+  return knex.schema.createTable('users_groups', (table) => {
     // TABLE COLUMN DEFINITIONS HERE
     table.increments().primary()
     table.integer('users_id').references('id').inTable('users').notNull().onDelete('CASCADE')
-    table.integer('timelines_id').references('id').inTable('timelines').notNull().onDelete('CASCADE')
+    table.integer('groups_id').references('id').inTable('groups').notNull().onDelete('CASCADE')
     table.timestamps(true, true)
   })
 }
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('users_timelines')
+  return knex.schema.dropTableIfExists('users_groups')
 }
