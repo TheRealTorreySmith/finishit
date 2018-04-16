45ccdd0d4a70a0b435fa334417623771c8db626f
Torrey Smith <torreypsmith@aol.com>
2018-04-16 16:08:19 -0600

    carousel clickers

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,10 +1,15 @@
+const nextButton = () => {
+  $('.carousel').carousel('next')
+}
 
-
+const lastButton = () => {
+  $('.carousel').carousel('prev')
+}
 
 $(document).ready(() => {
   $('.dropdown-trigger').dropdown();
   $('.carousel').carousel()
-
-
+  $('.next').click(nextButton)
+  $('.prev').click(lastButton)
 
 })
