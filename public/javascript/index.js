16ee7417c5ee24e6ec06f76ee0bf659cbdcf1fc9
Nathan Burgo <nthnbrg185@gmail.com>
2018-04-16 11:30:14 -0600

    Fixed timeline errors on timeline.ejs/timeline.js. Some colors updated on buttons on index.ejs

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -4,20 +4,31 @@ $(document).ready(() => {
 
   // Create a DataSet (allows two way data-binding)
   const items = new vis.DataSet([
-    { id: 1, content: 'Do Stuff', start: '2018-04-10', end: '2018-04-20' },
-    { id: 2, content: 'item 2', start: '2018-04-14' },
-    { id: 3, content: 'item 3', start: '2018-04-18' },
-    { id: 4, content: 'item 4', start: '2018-04-16', end: '2018-04-19' },
-    { id: 5, content: 'item 5', start: '2018-04-25' },
-    { id: 6, content: 'item 6', start: '2018-04-27' }
+    {
+      id: 1,
+      content: 'Do Stuff',
+      start: '2018-04-10',
+      end: '2018-04-20'
+    },
+    {
+      id: 2,
+      content: 'File Taxes',
+      start: '2018-04-16',
+      end: '2018-04-18'
+    }
+
   ])
 
-  const options = {}
+  const options = {
+    configure: true,
+    editable: true,
+    selectable: true
+  }
 
   // Create a Timeline
   const timeline = new vis.Timeline(container, items, options)
 })
-// 
+//
 // $('#login').on('click', (event) => {
 //
 // })
--- public/javascript/timeline.js
+++ public/javascript/timeline.js
@@ -1,35 +1,23 @@
-// DOM element where the Timeline will be attached
-const container = $('#visualization');
+$(document).ready(() => {
+  // DOM element where the Timeline will be attached
+  const container = document.getElementById('visual')
 
-// Create a DataSet (allows two way data-binding)
-const items = new vis.DataSet([
-  {id: 1, content: 'item 1', start: '2013-04-20'},
-  {id: 2, content: 'item 2', start: '2013-04-14'},
-  {id: 3, content: 'item 3', start: '2013-04-18'},
-  {id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19'},
-  {id: 5, content: 'item 5', start: '2013-04-25'},
-  {id: 6, content: 'item 6', start: '2013-04-27'}
-])
+  // Create a DataSet (allows two way data-binding)
+  const items = new vis.DataSet([
+    { id: 1, content: 'item 1', start: '2013-04-20' },
+    { id: 2, content: 'item 2', start: '2013-04-14' },
+    { id: 3, content: 'item 3', start: '2013-04-18' },
+    { id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19' },
+    { id: 5, content: 'item 5', start: '2013-04-25' },
+    { id: 6, content: 'item 6', start: '2013-04-27' }
+  ])
 
-// Configuration for the Timeline
-const options = {
-  width: '100%',
-  align: center,
-  autoResize: true,
-  multiselect: true,
-  editable: {
-    add: true,
-    remove: true,
-    updateGroup: true,
-    updateTime: true,
-    overrideTimes: true
-  },
-  selectable: true,
-  height: '30px',
-  margin: {
-    item: 20
+  // Configuration for the Timeline
+  const options = {
+    selectable: true,
+    editable: true
   }
-}
 
-// Create a Timeline
-const timeline = new vis.Timeline(container, items, options)
+  // Create a Timeline
+  const timeline = new vis.Timeline(container, items, options)
+})
