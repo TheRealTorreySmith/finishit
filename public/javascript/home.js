b4dd51dbb1afe0aba66fc28ac1f558df9c07be90
Torrey Smith <torreypsmith@aol.com>
2018-04-17 06:10:09 -0600

    carousel modals

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,3 +1,4 @@
+//FUNCTIONS CALLED BY EVENT HANDLERS
 const nextButton = () => {
   $('.carousel').carousel('next')
 }
@@ -6,10 +7,21 @@ const lastButton = () => {
   $('.carousel').carousel('prev')
 }
 
+const numButton = (event) => {
+  const num = $(event.target)[0].innerHTML
+  $('.carousel').carousel('set', num)
+
+}
+
+$('.modal').modal();
+
+// DOCUMENT READY
 $(document).ready(() => {
+  // EVENT HANDLERS
   $('.dropdown-trigger').dropdown();
   $('.carousel').carousel()
   $('.next').click(nextButton)
   $('.prev').click(lastButton)
+  $('.btn-small').click(numButton)
 
 })
