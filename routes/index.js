6becab612ad0d8b72db144ff68f08841ae6dc8c2
Nathan Burgo <nthnbrg185@gmail.com>
2018-04-18 10:47:34 -0600

    If user has an authenticated cookie, redirect them to their dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const knex = require('../knex')
+const jwt = require('jsonwebtoken')
+const env = require('dotenv').config()
+
+const KEY = process.env.JWT_KEY
+
+const getCookie = (req, res, next) => {
+  if (req.cookies.token) {
+    const payload = jwt.verify(req.cookies.token, KEY)
+    knex('users')
+      .select('id')
+      .where('username', payload.username)
+      .then((result) => {
+        res.json({ message: 'Success', payload, id: result[0].id })
+      })
+  } else {
+    res.json({ message: 'Failed' })
+  }
+}
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'finishit' });
-});
+})
+
+router.get('/cookie', getCookie)
 
 module.exports = router;
