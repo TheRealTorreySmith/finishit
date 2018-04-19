919b1af7cc1543d1cce7b9aef625a5a7c72fd601
Torrey Smith <torreypsmith@aol.com>
2018-04-19 11:42:50 -0600

    timeline stuff

--- routes/timeline.js
+++ routes/timeline.js
@@ -9,25 +9,25 @@ const env = require('dotenv').config()
 const KEY = process.env.JWT_KEY
 
 /* GET TIMELINE PAGE */
-const selectedTimelinePage = (req, res, next) => {
-  knex.from('timelines')
-    .join('users_timelines', 'users_timelines.timelines_id', 'timelines.id')
-    .join('users', 'users_timelines.users_id', 'users.id')
-    .where('users_timelines.timelines_id', req.params.id)
-    .returning(['name', 'timeAxis', 'description'])
-    .then((result) => {
-      console.log(result)
-      const selectedDuration = result[0].timeAxis.scale
-      const timelineName = result[0].name
-      const timelineDescription = result[0].description
-      res.render('timeline', {
-        title: ' The Selected Timeline Page',
-        selectedDuration,
-        timelineName,
-        timelineDescription
-      })
-    })
-}
+// const selectedTimelinePage = (req, res, next) => {
+//   knex.from('timelines')
+//     .join('users_timelines', 'users_timelines.timelines_id', 'timelines.id')
+//     .join('users', 'users_timelines.users_id', 'users.id')
+//     .where('users_timelines.timelines_id', req.params.id)
+//     .returning(['name', 'timeAxis', 'description'])
+//     .then((result) => {
+//       console.log(result)
+//       const selectedDuration = result[0].timeAxis.scale
+//       const timelineName = result[0].name
+//       const timelineDescription = result[0].description
+//       res.render('timeline', {
+//         title: ' The Selected Timeline Page',
+//         selectedDuration,
+//         timelineName,
+//         timelineDescription
+//       })
+//     })
+// }
 
 const getTimelineData = (req, res, next) => {
   const payload = jwt.verify(req.cookies.fstoken, KEY)
@@ -56,7 +56,7 @@ const createNewEvent = (req, res, next) => {
 
 // ROUTE REQUESTS
 router.get('/:id/getTimeline', getTimelineData)
-router.get('/:id', selectedTimelinePage)
+// router.get('/:id', selectedTimelinePage)
 router.post('/:id/newevent', createNewEvent)
 
 // EXPORTS
